2097771ad29cda2694749c764642035c0d8279dd
andresreyesg4 <andres.reyes121014@gmail.com>
2025-06-20 22:45:57 -0400

    starting solution

--- Java/Easy/Roman_to_Integer/SolutionA.java
+++ Java/Easy/Roman_to_Integer/SolutionA.java
@@ -1,5 +1,8 @@
 package Java.Easy.Roman_to_Integer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SolutionA {
-    
+    HashMap<Integer,String> romanNumberals = new HashMap<>();
 }
