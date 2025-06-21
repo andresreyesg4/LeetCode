e2f4a180b7cc7369a5f22a153173e9a502b64d3d
andresreyesg4 <andres.reyes121014@gmail.com>
2025-06-21 16:56:14 -0400

    First part solved

--- Java/Easy/Roman_to_Integer/SolutionA.java
+++ Java/Easy/Roman_to_Integer/SolutionA.java
@@ -4,5 +4,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SolutionA {
-    HashMap<Integer,String> romanNumberals = new HashMap<>();
+    
+
+    public static void main(String[] args) {
+        HashMap<String,Integer> romanNumerals = new HashMap<>();
+        romanNumerals.put("I", 1);
+        romanNumerals.put("V", 5);
+        romanNumerals.put("X", 10);
+        romanNumerals.put("L", 50);
+        romanNumerals.put("C", 100);
+        romanNumerals.put("D", 500);
+        romanNumerals.put("M",1000);
+
+        int romanNumber = generateRomanNumeral(romanNumerals, "V");
+        println(romanNumber);
+    }
+
+    public static int generateRomanNumeral(HashMap map, String rm){
+        int result = 0;
+        if(map.get(rm) != null){
+            result = (int)map.get(rm);
+        }else{
+            
+        }
+        return result;
+    }
+
+    public static void println(Object obj){
+        System.out.println(obj);
+    }
+   
+
 }
