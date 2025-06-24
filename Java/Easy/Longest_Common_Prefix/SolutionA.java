cff6189b4182e7ed08f78bb9f4956d3cbddb678f
andresreyesg4 <andres.reyes121014@gmail.com>
2025-06-23 23:48:20 -0400

    Solved another one

---
+++ Java/Easy/Longest_Common_Prefix/SolutionA.java
@@ -0,0 +1,60 @@
+package Java.Easy.Longest_Common_Prefix;
+
+import java.util.HashMap;
+
+public class SolutionA {
+    
+    public static void main(String[] args) {
+        SolutionA solution = new SolutionA();
+        String[] strs = {"cir","car"};
+        // String[] strs = {"bab","bcc"};
+        solution.logestCommonPrefix(strs);
+
+    }
+
+    public String logestCommonPrefix(String[] strs){
+        StringBuilder lcp = new StringBuilder();
+        if(strs.length < 1 ){
+            return "";
+        }else{
+            String smallest = strs[0];
+            String strtemp = "";
+            int smallestLength = strs[0].length();
+            int smallesIndex = 0;
+            for(String str: strs){
+                int temp = str.length();
+                if(temp < smallestLength){
+                    smallestLength = temp;
+                    smallest = str;
+                }
+            }
+            int numberOfHits = 0;
+            for(int i = 0; i < smallest.length(); i++){
+                for(int j = 0; j < strs.length; j++){
+                    if(smallest == strs[j]){
+                        continue;
+                    }else{
+                        if(smallest.charAt(i) == strs[j].charAt(i)){
+                            numberOfHits++;
+                        }else if(i == 0 && smallest.charAt(i) != strs[j].charAt(i)){
+                            return "";
+                        }else if(smallest.charAt(i) != strs[j].charAt(i)){
+                            return lcp.toString();
+                        }
+                    }
+                }
+                if(numberOfHits == strs.length -1){
+                    lcp.append(smallest.charAt(i));
+                }
+                numberOfHits = 0;
+                strtemp = lcp.toString();
+            }
+        }
+        println(lcp.toString());
+        return lcp.toString();
+    }
+
+    public static void println(Object obj){
+        System.out.println(obj);
+    }
+}
