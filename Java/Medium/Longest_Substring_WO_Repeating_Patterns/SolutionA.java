e3a68a32187e4b6737f9a5f3dc4f9a56392be242
andresreyesg4 <andres.reyes121014@gmail.com>
2025-07-07 22:54:36 -0400

    Added new problem

---
+++ Java/Medium/Longest_Substring_WO_Repeating_Patterns/SolutionA.java
@@ -0,0 +1,11 @@
+package Java.Medium.Longest_Substring_WO_Repeating_Patterns;
+
+import java.util.Set;
+
+public class SolutionA {
+    public int lengthOfLongestSubstring(String s) {
+        int length = 0;
+        return length;
+    }
+
+}
